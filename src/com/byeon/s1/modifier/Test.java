b9bf421ec27a3867893b83bb3fde031a3cf52f9f
BJSSSSSS <john0222223@gmail.com>
2022-01-06 14:04:08 +0900

    modify(2022.01.06)1

---
+++ src/com/byeon/s1/modifier/Test.java
@@ -0,0 +1,15 @@
+
+
+package com.byeon.s1.modifier;
+
+class Test {
+
+	private int num;
+	
+	public Test() {
+		
+	}
+	
+}
+
+
---
+++ src/com/byeon/s2/Test2.java
@@ -0,0 +1,17 @@
+
+
+package com.byeon.s2;
+
+public class Test2 {
+
+	int num2; // default
+	public int num3;
+	
+	public Test2() {
+		num2 = 20;
+		System.out.println(num2);
+	}
+	
+}
+
+
